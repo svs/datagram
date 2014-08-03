c6f7d7652bc1fa2a47b539efd6171d817c1f1581
svs <svs@svs.io>
2014-08-03 19:26:04 +0530

    rabbitmq_bigwig setup
    Former-commit-id: cd8bb0c6c39fe23ba431c4be2fd3d46d0a6bf31a

--- config/initializers/rabbitmq.rb
+++ config/initializers/rabbitmq.rb
@@ -1,14 +1,18 @@
 if Rails.application.secrets.rabbitmq_url
   $conn = Bunny.new(Rails.application.secrets.rabbitmq_url)
+elsif ENV['RABBITMQ_BIGWIG_TX_URL']
+  $tx_conn = Bunny.new(ENV['RABBITMQ_BIGWIG_TX_URL'])
+  $rx_conn = Bunny.new(ENV['RABBITMQ_BIGWIG_RX_URL'])
 else
   $conn = Bunny.new
 end
 
 $conn.start
 
-$ch = $conn.create_channel
-$watches  = $ch.queue("watches", :durable => true)
-$watch_responses  = $ch.queue("watch_responses", :durable => true)
-$datagrams =  $ch.queue("datagrams", :durable => true)
-$datagram_responses =  $ch.queue("datagram_responses", :durable => true)
-$x  = $ch.default_exchange
+$ch_tx = ($conn || $tx_conn).create_channel
+$ch_rx = ($conn || $rx_conn).create_channel
+$watches  = $ch_tx.queue("watches", :durable => true)
+$watch_responses  = $ch_rx.queue("watch_responses", :durable => true)
+$datagrams =  $ch_tx.queue("datagrams", :durable => true)
+$datagram_responses =  $ch_rx.queue("datagram_responses", :durable => true)
+$x  = $ch_tx.default_exchange
