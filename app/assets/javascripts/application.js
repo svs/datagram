cad5488bfd75d12ac977cbc7c475b0588f5175a5
svs <svs@svs.io>
2014-08-03 20:21:45 +0530

    adding worker.js
    Former-commit-id: 0570e5991b7312517fc9d17428a9b8923fc3b953

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require angular-highlightjs.js
 //= require ace/ace.js
 //= require ui-ace.js
+//= require ace/worker/worker.js
 //= require ace/mode-json.js
 //= require ace/mode-sql.js
 //= require checklist-model.js
