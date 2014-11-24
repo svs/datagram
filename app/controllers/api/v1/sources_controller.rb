4efd387cfda65d66eb1879dc995d42a73ced290b
svs <svs@svs.io>
2014-11-24 19:57:51 +0530

    adds source editing
    Former-commit-id: d370b8e22a6814375fbb496f0d7c53cea6cbb597

--- app/controllers/api/v1/sources_controller.rb
+++ app/controllers/api/v1/sources_controller.rb
@@ -8,6 +8,30 @@ module Api
         render json: @sources
       end
 
+      def show
+        render json: policy_scope(Source).find(params[:id])
+      end
+
+
+      def update
+        source = policy_scope(Source).find(params[:id])
+        if source
+          if source.update(source_params)
+            render json: source
+          else
+            render json: source.errors, status: 422
+          end
+        else
+          render json: {}, status: 404
+        end
+      end
+
+      private
+
+      def source_params
+        params.require(:source).permit(:name, :url)
+      end
+
     end
   end
 end
