69fdd48ab887c49b58d34cf8af44eea026376f8b
svs <svs@svs.io>
2015-01-29 08:52:59 +0530

    adds ui for routing key
    Former-commit-id: f50caaa40f9bdbb39a0dbd0fb318f6b4bd7f0a08

--- app/models/datagram.rb
+++ app/models/datagram.rb
@@ -4,6 +4,7 @@ class Datagram < ActiveRecord::Base
   belongs_to :source
 
   before_save :make_token_and_slug
+
   include Rails.application.routes.url_helpers
 
   def as_json(include_root = false, max_size = Float::INFINITY)
