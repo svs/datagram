5b64a3555f24785b84a2d99b536dffcaad8167f2
Siddharth Sharma <svs@svs.io>
2016-11-28 07:23:48 +0530

    fixing clock

--- app/services/datagram_finder.rb
+++ app/services/datagram_finder.rb
@@ -4,9 +4,10 @@ class DatagramFinder
     Rails.logger.info("#DatagramFinder starting")
     ds = Datagram.where('archived IS DISTINCT FROM true').all.map{|d|
       (d.param_sets || {}).select{|n,v|
-        (v["frequency"].to_i > 0 || !p["at"].blank?) rescue nil
-      }.map{|k,m| o = OpenStruct.new(m.merge("id" => d.id)); o.frequency = o.frequency.to_i; o}
+        (v["frequency"].to_i > 0 || !v["at"].blank?) rescue nil
+      }.map{|k,m| o = OpenStruct.new(m.merge("id" => d.id)); o.frequency = o.frequency.to_i; o.name = "#{d.name} - #{o.name}";o}
     }.select{|k| !k.blank?}.flatten.tap{|x|
+      ap x
       Rails.logger.info("#DatagramFinder found #{x.count} datagrams with refresh turned on")
       x.map{|y| Rails.logger.info "#DatagramFinder -- #{Datagram.find(y.id).name} #{y.name} #{y.frequency}"}
     }
