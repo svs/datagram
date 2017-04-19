528ef3170f567e744a3edf2b058dad05fa8d9113
Siddharth Sharma <svs@svs.io>
2017-04-19 23:24:16 +0530

    hoping and praying

--- app/assets/javascripts/datagrams.js
+++ app/assets/javascripts/datagrams.js
@@ -1,3 +1,4 @@
+//= require tauCharts.min.js
 //= require angular-pusher
 //= require 'checklist-model.js'
 //= require highlight.pack.js
@@ -11,7 +12,8 @@
 //= require handsontable.min.js
 //= require hightchart_renderers.js
 //= require novix.pivot.renderer.js
-//= require tauCharts.min.js
+
+
 agGrid.LicenseManager.setLicenseKey("ag-Grid_Evaluation_License_Not_for_Production_100Devs24_May_2017__MTQ5NTU4MDQwMDAwMA==16be8f8f82a5e4b5fa39766944c69a32");
 agGrid.initialiseAgGridWithAngular1(angular);
 var datagramsApp = angular.module('datagramsApp', ['restangular','ui.router','checklist-model', 'hljs', 'doowb.angular-pusher', 'directives.json','ui.bootstrap', "pascalprecht.translate", "humanSeconds","ui.ace","highcharts-ng","ngSanitize","agGrid",'ngCsv','angular-pivottable']).
