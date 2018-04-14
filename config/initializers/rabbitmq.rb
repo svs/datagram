e7bebf2eae2d90f06d09c9fab3c436d5b984ebce
Siddharth Sharma <siddharth.sharma@peopleinteractive.in>
2018-04-15 00:46:34 +0530

    fuck docker

--- config/initializers/rabbitmq.rb
+++ config/initializers/rabbitmq.rb
@@ -1,21 +1,27 @@
-begin
-  if ENV['RABBITMQ_PORT']
-    endpoint=ENV['RABBITMQ_PORT'].gsub("tcp","amqp")
-  else
-    endpoint = Rails.application.secrets.rabbitmq_url || ENV['RABBITMQ_URL'] || "amqp://localhost:5672"
+5.times do
+  begin
+    if ENV['RABBITMQ_PORT']
+      endpoint=ENV['RABBITMQ_PORT'].gsub("tcp","amqp")
+    else
+      endpoint = Rails.application.secrets.rabbitmq_url || ENV['RABBITMQ_URL'] || "localhost://rabbitmq:5672"
+    end
+    username = Rails.application.secrets.rabbitmq_user || "guest"
+    password = Rails.application.secrets.rabbitmq_password || "guest"
+
+    $conn = Bunny.new(endpoint)
+
+    $conn.start
+
+    $ch = $conn.create_channel
+    $watches  = $ch.queue("watches", :durable => true)
+    $watch_responses  = $ch.queue(Rails.application.secrets["watch_response_q"] || "watch_responses", :durable => true)
+    $x  = $ch.topic('datagrams_topic_exchange', auto_delete: false)
+
+    $watches.bind($x)
+    break
+  rescue
+    puts "..."
+    sleep 3
+    puts "#sleepiung for 3 seconds"
   end
-  username = Rails.application.secrets.rabbitmq_user || "guest"
-  password = Rails.application.secrets.rabbitmq_password || "guest"
-
-  $conn = Bunny.new(endpoint)
-
-  $conn.start
-
-  $ch = $conn.create_channel
-  $watches  = $ch.queue("watches", :durable => true)
-  $watch_responses  = $ch.queue(Rails.application.secrets["watch_response_q"] || "watch_responses", :durable => true)
-  $x  = $ch.topic('datagrams_topic_exchange', auto_delete: false)
-
-  $watches.bind($x)
-rescue
 end
