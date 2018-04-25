538f14417737308691a6ed61bc991aec23e5118c
root <root@ip-10-0-0-170.ec2.internal>
2018-04-25 13:09:56 +0000

    updated db names

--- app/services/watch_response_handler.rb
+++ app/services/watch_response_handler.rb
@@ -5,6 +5,7 @@ class WatchResponseHandler
   end
 
   def handle!
+    ap params
     context = {datagram: (datagram.token rescue ""), watch: (watch.token rescue nil), timestamp: timestamp}
     DgLog.new("#WatchResponseHandler processing: #{params[:id]}", context).log
     if wr
